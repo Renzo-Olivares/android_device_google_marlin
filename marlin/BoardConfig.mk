d7ed1e67dfee0476b3856ee4a975091a91910b21
Renzo-Olivares <renzo.olivares1@gmail.com>
2017-04-28 14:38:22 -0700

    Revert "Revert "Revert "Revert "Revert "test"""""
    This reverts commit 362af82390412e2aaf2035a8929302d82c8117c1.

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -109,15 +109,6 @@ BOARD_SECCOMP_POLICY += device/google/marlin/seccomp
 
 BOARD_EGL_CFG := device/google/marlin/egl.cfg
 
-# Kernel
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CONFIG := marlin_defconfig
-TARGET_KERNEL_SOURCE := kernel/google/marlin
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-
-
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000
