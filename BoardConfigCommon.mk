8357acada24111f041b32b07885cbe524c803c38
Renzo-Olivares <renzo.olivares1@gmail.com>
2017-04-27 18:58:35 -0700

    test

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,11 +1,3 @@
-# Kernel
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CONFIG := marlin_defconfig
-TARGET_KERNEL_SOURCE := kernel/google/marlin
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
